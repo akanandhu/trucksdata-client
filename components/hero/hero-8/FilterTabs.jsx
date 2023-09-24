7272e3e76f71dd44c63e6e070b876598144612c1
“neeraj” <neerajramachandran30@gmail.com>
2023-09-24 16:54:07 +0530

    feat/filter data mapping

--- components/hero/hero-8/FilterTabs.jsx
+++ components/hero/hero-8/FilterTabs.jsx
@@ -3,22 +3,24 @@ import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import SearchButton from "./SearchButton";
 import DropInput from "./DropInput";
+import { dropdown } from "./DropdownItems";
 function FilterTabs() {
-  const itemList = [
-    "Manufacturer",
-    "Vehicle Class",
-    "Application",
-    "GVW",
-    "Power Source",
-  ];
+  // const itemList = [
+  //   "Manufacturer",
+  //   "Vehicle Class",
+  //   "Application",
+  //   "GVW",
+  //   "Power Source",
+  // ];
+
   return (
     <>
       <Tabs className="tabs -underline-2 js-tabs">
         <TabList className="tabs__controls row x-gap-40 y-gap-10 lg:x-gap-20 pb-30 js-tabs-controls">
-          {itemList.map((item, i) => (
+          {dropdown.map((item, i) => (
             <Tab className="col-auto" key={i}>
               <button className="tabs__button text-light-1 fw-500 js-tabs-button">
-                {item}
+                {item.tabItem}
               </button>
             </Tab>
           ))}
@@ -26,11 +28,12 @@ function FilterTabs() {
         {/* End tab-controls */}
 
         <div className="tabs__content js-tabs-content">
-          {itemList.map((item) => (
-            <TabPanel key={item}>
+          {dropdown.map((item) => (
+            <TabPanel>
               <div className="d-flex p-2 bd-highlight justify-content-between">
-                <DropInput />
-                <DropInput />
+                {item.dropdownItem.map((dropdownDetails) => (
+                  <DropInput dropdownDetails={dropdownDetails} />
+                ))}
                 <SearchButton />
               </div>
             </TabPanel>
