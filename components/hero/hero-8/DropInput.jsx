6af69c614b9fe732f9df63ea46cf93578e687a5e
“neeraj” <neerajramachandran30@gmail.com>
2023-09-24 16:54:07 +0530

    feat/filter data mapping

--- components/hero/hero-8/DropInput.jsx
+++ components/hero/hero-8/DropInput.jsx
@@ -1,36 +1,9 @@
 import { useState } from "react";
 import { BiSolidFactory } from "react-icons/bi";
-function DropInput() {
+function DropInput(props) {
+  const { dropdownDetails } = props;
   const [searchValue, setSearchValue] = useState("");
   const [selectedItem, setSelectedItem] = useState(null);
-
-  const locationSearchContent = [
-    {
-      id: 1,
-      name: "London",
-      address: "Greater London, United Kingdom",
-    },
-    {
-      id: 2,
-      name: "New York",
-      address: "New York State, United States",
-    },
-    {
-      id: 3,
-      name: "Paris",
-      address: "France",
-    },
-    {
-      id: 4,
-      name: "Madrid",
-      address: "Spain",
-    },
-    {
-      id: 5,
-      name: "Santorini",
-      address: "Greece",
-    },
-  ];
 
   const handleOptionClick = (item) => {
     setSearchValue(item.name);
@@ -46,16 +19,16 @@ function DropInput() {
           data-bs-offset="0,22"
         >
           <div className="d-flex justify-content-between align-items-center">
-            <div className="w-25">
-              <BiSolidFactory style={{ fontSize: "2.2em" }} />
-            </div>
+            <div className="w-25">{dropdownDetails.icon}</div>
             <div>
-              <h4 className="text-15 fw-500 ls-2 lh-16">Manufacturer</h4>
+              <h4 className="text-15 fw-500 ls-2 lh-16">
+                {dropdownDetails.title}
+              </h4>
               <div className="text-15 text-light-1 ls-2 lh-16">
                 <input
                   autoComplete="off"
                   type="search"
-                  placeholder="Select Manufacturer"
+                  placeholder={dropdownDetails.placeholder}
                   className="js-search js-dd-focus"
                   value={searchValue}
                   onChange={(e) => setSearchValue(e.target.value)}
@@ -68,7 +41,7 @@ function DropInput() {
         <div className="shadow-2 dropdown-menu min-width-400">
           <div className="bg-white px-20 py-20 sm:px-0 sm:py-15 rounded-4">
             <ul className="y-gap-5 js-results">
-              {locationSearchContent.map((item) => (
+              {dropdownDetails.items.map((item) => (
                 <li
                   className={`-link d-block col-12 text-left rounded-4 px-20 py-15 js-search-option mb-1 ${
                     selectedItem && selectedItem.id === item.id ? "active" : ""
@@ -78,14 +51,14 @@ function DropInput() {
                   onClick={() => handleOptionClick(item)}
                 >
                   <div className="d-flex">
-                    <div className="icon-location-2 text-light-1 text-20 pt-4" />
+                    {/* <div className="icon-location-2 text-light-1 text-20 pt-4" /> */}
                     <div className="ml-10">
                       <div className="text-15 lh-12 fw-500 js-search-option-target">
                         {item.name}
                       </div>
-                      <div className="text-14 lh-12 text-light-1 mt-5">
+                      {/* <div className="text-14 lh-12 text-light-1 mt-5">
                         {item.address}
-                      </div>
+                      </div> */}
                     </div>
                   </div>
                 </li>
---
+++ components/hero/hero-8/DropdownItems.js
@@ -0,0 +1,356 @@
+import { BiSolidFactory } from "react-icons/bi";
+import { FaTruckMoving } from "react-icons/fa";
+const dropdown = [
+  {
+    id: 1,
+    tabItem: "Manufacturer",
+    dropdownItem: [
+      {
+        title: "Manufacturer",
+        placeholder: "Select Manufacturer",
+        icon: <BiSolidFactory style={{ fontSize: "2.2em" }} />,
+        items: [
+          {
+            id: 1,
+            name: "Asok Leyland",
+            // address: "Greater London, United Kingdom",
+          },
+          {
+            id: 2,
+            name: "Tata Motors",
+            // address: "New York State, United States",
+          },
+          {
+            id: 3,
+            name: "Volvo",
+            // address: "France",
+          },
+          {
+            id: 4,
+            name: "Eicher",
+            // address: "Spain",
+          },
+          {
+            id: 5,
+            name: "Bharat Benz",
+            // address: "Greece",
+          },
+        ],
+      },
+      {
+        title: "Model",
+        placeholder: "Select Model",
+        icon: <FaTruckMoving style={{ fontSize: "2.2em" }} />,
+        items: [
+          {
+            id: 1,
+            name: "Model 1",
+            // address: "Greater London, United Kingdom",
+          },
+          {
+            id: 2,
+            name: "Model 2",
+            // address: "New York State, United States",
+          },
+          {
+            id: 3,
+            name: "Model 3",
+            // address: "France",
+          },
+          {
+            id: 4,
+            name: "Model 4",
+            // address: "Spain",
+          },
+          {
+            id: 5,
+            name: "Model 5",
+            // address: "Greece",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    tabItem: "Body Type",
+    dropdownItem: [
+      {
+        title: "Body Type",
+        placeholder: "Select Body Type",
+        icon: <BiSolidFactory style={{ fontSize: "2.2em" }} />,
+        items: [
+          {
+            id: 1,
+            name: "Asok Leyland",
+            // address: "Greater London, United Kingdom",
+          },
+          {
+            id: 2,
+            name: "Tata Motors",
+            // address: "New York State, United States",
+          },
+          {
+            id: 3,
+            name: "Volvo",
+            // address: "France",
+          },
+          {
+            id: 4,
+            name: "Eicher",
+            // address: "Spain",
+          },
+          {
+            id: 5,
+            name: "Bharat Benz",
+            // address: "Greece",
+          },
+        ],
+      },
+      {
+        title: "Model",
+        placeholder: "Select Model",
+        icon: <FaTruckMoving style={{ fontSize: "2.2em" }} />,
+        items: [
+          {
+            id: 1,
+            name: "Model 1",
+            // address: "Greater London, United Kingdom",
+          },
+          {
+            id: 2,
+            name: "Model 2",
+            // address: "New York State, United States",
+          },
+          {
+            id: 3,
+            name: "Model 3",
+            // address: "France",
+          },
+          {
+            id: 4,
+            name: "Model 4",
+            // address: "Spain",
+          },
+          {
+            id: 5,
+            name: "Model 5",
+            // address: "Greece",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    tabItem: "Application",
+    dropdownItem: [
+      {
+        title: "Body Type",
+        placeholder: "Select Body Type",
+        icon: <BiSolidFactory style={{ fontSize: "2.2em" }} />,
+        items: [
+          {
+            id: 1,
+            name: "Asok Leyland",
+            // address: "Greater London, United Kingdom",
+          },
+          {
+            id: 2,
+            name: "Tata Motors",
+            // address: "New York State, United States",
+          },
+          {
+            id: 3,
+            name: "Volvo",
+            // address: "France",
+          },
+          {
+            id: 4,
+            name: "Eicher",
+            // address: "Spain",
+          },
+          {
+            id: 5,
+            name: "Bharat Benz",
+            // address: "Greece",
+          },
+        ],
+      },
+      {
+        title: "Model",
+        placeholder: "Select Model",
+        icon: <FaTruckMoving style={{ fontSize: "2.2em" }} />,
+        items: [
+          {
+            id: 1,
+            name: "Model 1",
+            // address: "Greater London, United Kingdom",
+          },
+          {
+            id: 2,
+            name: "Model 2",
+            // address: "New York State, United States",
+          },
+          {
+            id: 3,
+            name: "Model 3",
+            // address: "France",
+          },
+          {
+            id: 4,
+            name: "Model 4",
+            // address: "Spain",
+          },
+          {
+            id: 5,
+            name: "Model 5",
+            // address: "Greece",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    tabItem: "GVW",
+    dropdownItem: [
+      {
+        title: "Body Type",
+        placeholder: "Select Body Type",
+        icon: <BiSolidFactory style={{ fontSize: "2.2em" }} />,
+        items: [
+          {
+            id: 1,
+            name: "Asok Leyland",
+            // address: "Greater London, United Kingdom",
+          },
+          {
+            id: 2,
+            name: "Tata Motors",
+            // address: "New York State, United States",
+          },
+          {
+            id: 3,
+            name: "Volvo",
+            // address: "France",
+          },
+          {
+            id: 4,
+            name: "Eicher",
+            // address: "Spain",
+          },
+          {
+            id: 5,
+            name: "Bharat Benz",
+            // address: "Greece",
+          },
+        ],
+      },
+      {
+        title: "Model",
+        placeholder: "Select Model",
+        icon: <FaTruckMoving style={{ fontSize: "2.2em" }} />,
+        items: [
+          {
+            id: 1,
+            name: "Model 1",
+            // address: "Greater London, United Kingdom",
+          },
+          {
+            id: 2,
+            name: "Model 2",
+            // address: "New York State, United States",
+          },
+          {
+            id: 3,
+            name: "Model 3",
+            // address: "France",
+          },
+          {
+            id: 4,
+            name: "Model 4",
+            // address: "Spain",
+          },
+          {
+            id: 5,
+            name: "Model 5",
+            // address: "Greece",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 5,
+    tabItem: "Power Source",
+    dropdownItem: [
+      {
+        title: "Body Type",
+        placeholder: "Select Body Type",
+        icon: <BiSolidFactory style={{ fontSize: "2.2em" }} />,
+        items: [
+          {
+            id: 1,
+            name: "Asok Leyland",
+            // address: "Greater London, United Kingdom",
+          },
+          {
+            id: 2,
+            name: "Tata Motors",
+            // address: "New York State, United States",
+          },
+          {
+            id: 3,
+            name: "Volvo",
+            // address: "France",
+          },
+          {
+            id: 4,
+            name: "Eicher",
+            // address: "Spain",
+          },
+          {
+            id: 5,
+            name: "Bharat Benz",
+            // address: "Greece",
+          },
+        ],
+      },
+      {
+        title: "Model",
+        placeholder: "Select Model",
+        icon: <FaTruckMoving style={{ fontSize: "2.2em" }} />,
+        items: [
+          {
+            id: 1,
+            name: "Model 1",
+            // address: "Greater London, United Kingdom",
+          },
+          {
+            id: 2,
+            name: "Model 2",
+            // address: "New York State, United States",
+          },
+          {
+            id: 3,
+            name: "Model 3",
+            // address: "France",
+          },
+          {
+            id: 4,
+            name: "Model 4",
+            // address: "Spain",
+          },
+          {
+            id: 5,
+            name: "Model 5",
+            // address: "Greece",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export { dropdown };
