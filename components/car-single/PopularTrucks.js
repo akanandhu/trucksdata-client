22336343b86aa42f254d8a9a832fa3bdde29de37
“neeraj” <neerajramachandran30@gmail.com>
2023-09-30 14:18:20 +0530

    feat/view page

---
+++ components/car-single/PopularTrucks.js
@@ -0,0 +1,34 @@
+const trucks = [
+  {
+    id: 1,
+    model: "Ashok Leyland 2820 Tipper",
+    price: "Rs 29.01 - 30.04",
+    modelImage: "/img/cars/truck-demo.jpeg",
+  },
+  {
+    id: 2,
+    model: "Ashok Leyland 2820 Tipper",
+    price: "Rs 29.01 - 30.04",
+    modelImage: "/img/cars/truck-demo.jpeg",
+  },
+  {
+    id: 3,
+    model: "Ashok Leyland 2820 Tipper",
+    price: "Rs 29.01 - 30.04",
+    modelImage: "/img/cars/truck-demo.jpeg",
+  },
+  {
+    id: 4,
+    model: "Ashok Leyland 2820 Tipper",
+    price: "Rs 29.01 - 30.04",
+    modelImage: "/img/cars/truck-demo.jpeg",
+  },
+  {
+    id: 5,
+    model: "Ashok Leyland 2820 Tipper",
+    price: "Rs 29.01 - 30.04",
+    modelImage: "/img/cars/truck-demo.jpeg",
+  },
+];
+
+export { trucks };
