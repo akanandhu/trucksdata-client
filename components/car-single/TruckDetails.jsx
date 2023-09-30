cd80f99a238ce4b62ecede7295a36f5e049246a4
“neeraj” <neerajramachandran30@gmail.com>
2023-10-01 01:05:48 +0530

    feat/view page

---
+++ components/car-single/TruckDetails.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function TruckDetails() {
+  return (
+    <div className="px-30 py-30 rounded-4 border-light shadow-4 bg-white w-360 lg:w-full">
+      <div className="row y-gap-15 items-center justify-between">
+        <div className="col-auto">
+          
+        </div>
+        <div className="col-auto">
+          <div className="d-flex items-center">
+            {/* <div className="text-14 text-right mr-10"> */}
+              {/* <div className="lh-15 fw-500">Exceptional</div> */}
+              {/* <div className="lh-15 text-light-1"> */}
+                {/* {car?.numberOfReviews} reviews */}
+              {/* </div> */}
+            {/* </div> */}
+
+            {/* <div className="size-40 flex-center bg-yellow-1 rounded-4">e */}
+              {/* <div className="text-14 fw-600 text-dark-1"> */}
+                {/* {car?.ratings} */}
+                {/* </div> */}
+            {/* </div> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TruckDetails;
