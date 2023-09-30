dbe405d2bab347aa512577c175f7f51eb1ca392b
“neeraj” <neerajramachandran30@gmail.com>
2023-10-01 01:05:48 +0530

    feat/view page

--- pages/car/car-single/[id].jsx
+++ pages/car/car-single/[id].jsx
@@ -19,6 +19,7 @@ import FilterBox from "../../../components/car-single/filter-box";
 import Faq from "../../../components/faq/Faq";
 import MapPropertyFinder from "../../../components/car-single/MapPropertyFinder";
 import PopularModals from "../../../components/car-single/PopularModals";
+import TruckDetails from "../../../components/car-single/TruckDetails";
 
 const TourSingleV1Dynamic = () => {
   const router = useRouter();
@@ -53,6 +54,12 @@ const TourSingleV1Dynamic = () => {
               <div className="row y-gap-20 justify-between items-end">
                 <div className="col-auto">
                   <h1 className="text-30 sm:text-24 fw-600">{car?.title}</h1>
+                  <h6 className="text-18 fw-500 text-dark-1 ml-5">
+                    Rs 28.12 - 29.19 lakh{" "}
+                    <span className="text-12 fw-400 text-light-1 pl-5">
+                      Ex-showroom
+                    </span>
+                  </h6>
                   <div className="row x-gap-10 items-center pt-10">
                     <div className="col-auto">
                       {/* <div className="d-flex x-gap-5 items-center">
@@ -73,7 +80,6 @@ const TourSingleV1Dynamic = () => {
                     </div> */}
                   </div>
                 </div>
-                {/* End title and other info */}
 
                 <div className="col-auto">
                   <div className="row x-gap-10 y-gap-10">
@@ -83,7 +89,6 @@ const TourSingleV1Dynamic = () => {
                         Share
                       </button>
                     </div>
-                    {/* End .col */}
                     <div className="col-auto">
                       <button className="button px-15 py-10 -blue-1 bg-light-2">
                         <i className="icon-heart mr-10" />
@@ -92,9 +97,7 @@ const TourSingleV1Dynamic = () => {
                     </div>
                   </div>
                 </div>
-                {/* End col button group */}
               </div>
-              {/* End .row */}
 
               <div className="mt-40">
                 <SlideGallery />
@@ -104,55 +107,9 @@ const TourSingleV1Dynamic = () => {
 
             <div className="col-lg-4">
               <div className="d-flex justify-end flex-column">
-                <div className="px-30 py-30 rounded-4 border-light shadow-4 bg-white w-360 lg:w-full">
-                  <div className="row y-gap-15 items-center justify-between">
-                    <div className="col-auto">
-                      <div className="text-14 text-light-1">
-                        {/* From */}
-                        <span className="text-20 fw-500 text-dark-1 ml-5">
-                          Rs 28.12 - 29.19 lakh
-                        </span>
-                        <span className="text-12 fw-400 text-light-1 pl-5">
-                          Ex-showroom
-                        </span>
-                      </div>
-                      <a
-                        href="#"
-                        className="text-14 text-blue-1 underline px-2"
-                      >
-                        Get On Road Price
-                      </a>
-                    </div>
-                    {/* End .col-auto */}
-
-                    <div className="col-auto">
-                      <div className="d-flex items-center">
-                        <div className="text-14 text-right mr-10">
-                          <div className="lh-15 fw-500">Exceptional</div>
-                          <div className="lh-15 text-light-1">
-                            {car?.numberOfReviews} reviews
-                          </div>
-                        </div>
-                        {/* End div */}
-
-                        <div className="size-40 flex-center bg-yellow-1 rounded-4">
-                          <div className="text-14 fw-600 text-dark-1">
-                            {car?.ratings}
-                          </div>
-                        </div>
-                        {/* End div */}
-                      </div>
-                    </div>
-                    {/* End .col-auto */}
-                  </div>
-                  {/* End .row */}
-
-                  {/* <div className="row y-gap-20 pt-20">
-                    <FilterBox />
-                  </div> */}
-                  {/* End .row */}
-                </div>
-
+                <div>
+                    <TruckDetails/>
+                </div>    
                 <div className="py-20">
                   <PopularModals />
                 </div>
