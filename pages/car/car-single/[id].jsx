bfb075d77f35cbc696b549d074e9a9b4fc12c4e7
“neeraj” <neerajramachandran30@gmail.com>
2023-09-30 16:25:33 +0530

    feat/view responsive video player

--- pages/car/car-single/[id].jsx
+++ pages/car/car-single/[id].jsx
@@ -178,7 +178,7 @@ const TourSingleV1Dynamic = () => {
                 <Overview />
               </div>
             </div>
-            
+
             {/* End .col-lg-8 */}
           </div>
           {/* End .row */}
