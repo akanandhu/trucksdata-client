c92316385c6b725f0b2b06ff24dbbee29737d82c
akanandhu <akananthu11@gmail.com>
2023-10-02 16:38:25 +0530

    feat: compare bar add

---
+++ components/compare-bar/CompareBar.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { clearAllCompare } from "../../features/compare/compareSlice";
+
+const CompareBar = () => {
+  const compareItems = useSelector((store) => store.compare.vehicles);
+  const dispatch = useDispatch();
+
+  function handleDeselect() {
+    dispatch(clearAllCompare());
+  }
+
+  return compareItems.length ? (
+    <div className="compare-bar">
+      <div className="compare-info">
+        Selected 0{compareItems?.length} Variants to Compare
+      </div>
+      <div className="button-container">
+        <button onClick={handleDeselect} className="deselect-btn text-black">
+          Deselect All
+        </button>
+        <button className="compare-btn text-white">Compare</button>
+      </div>
+    </div>
+  ) : null;
+};
+
+export default CompareBar;
