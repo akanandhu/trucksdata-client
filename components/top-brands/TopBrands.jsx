eaa0d9fa343d97782f41a3114caccfb8d36b76ae
“neeraj” <neerajramachandran30@gmail.com>
2023-11-26 01:53:51 +0530

    slider

--- components/top-brands/TopBrands.jsx
+++ components/top-brands/TopBrands.jsx
@@ -34,92 +34,122 @@ const TopBrands = (props) => {
   }, [topBrandFilter]);
 
   return (
-    
-
-    <>
-      <Swiper
-        
-        spaceBetween={20}
-        modules={[Navigation, Pagination]}
-        navigation={{
-          nextEl: ".js-popular-brand-next",
-          prevEl: ".js-popular-brand-prev",
-        }}
-        pagination={{
-          el: ".js-brand-pag_active",
-          clickable: true,
-        }}
-        breakpoints={{
-          500: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 2,
-            spaceBetween: 22,
-          },
-          1024: {
-            slidesPerView: 3,
-          },
-          1200: {
-            slidesPerView: 4,
-          },
-        }}
-      >
-        {brandDetails?.manufacturers?.slice(0, 8).map((item) => (
-          <SwiperSlide key={item.id + item.name}>
-            <Link
-              href={`/brands/${item.id}`}
-              // className="carCard -type-1 d-block rounded-4 hover-inside-slider"
-              className="citiesCard -type-3 d-block text-center"
-              data-aos="fade"
-              // data-aos-delay={item.delayAnimation}
+    <Slider {...settings}>
+      {console.log("Manufacturers ", brandDetails)}
+      {brandDetails?.manufacturers?.slice(0, 8).map((item) => (
+        <div
+          // className="col-xl-2  col-lg-3 col-sm-8 border-2 truck-card p-3 rounded-3 d-flex justify-content-center"
+          // className="bg-primary w-25"
+          key={i}
+          data-aos="fade"
+          data-aos-delay={i * 100}
+        >
+          <Link
+            href={`/brands/${item.id}`}
+            // className="citiesCard -type-3 d-block text-center"
+          >
+            <div
+            // className="citiesCard__image size-60 rounded-full mx-auto"
             >
-              <div
-                key={item.id + item.name}
-                data-aos="fade"
-                // data-aos-delay={item?.delayAnimation}
-                className="brand-card"
-              >
-                <div
-                  //  className="carCard__image truck-card-zoom"
-                  className="brand-card__image rounded-full mx-auto"
-                >
-                  <img
-                    width={300}
-                    height={300}
-                    className="js-lazy"
-                    src={item.logo[0].thumbnail}
-                    alt="image"
-                  />
-                </div>
-              </div>
-            </Link>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-
-      <div className="d-flex x-gap-15 items-center justify-center pt-20 sm:pt-20">
-        <div className="col-auto">
-          <button className="d-flex items-center text-24 arrow-left-hover js-popular-brand-prev">
-            <i className="icon icon-arrow-left" />
-          </button>
+              {/* <img
+                className="object-cover js-lazy"
+                src={item.logo[0].thumbnail}
+                // src={item.logo}
+                alt="image"
+                width={100}
+                height={100}
+              /> */}
+              crd
+            </div>
+          </Link>
         </div>
-        {/* End arrow prev */}
+      ))}
+    </Slider>
 
-        <div className="col-auto">
-          <div className="pagination -dots text-border js-brand-pag_active" />
-        </div>
-        {/* End arrow pagination */}
+    // <>
+    //   <Swiper
 
-        <div className="col-auto">
-          <button className="d-flex items-center text-24 arrow-right-hover js-popular-brand-next">
-            <i className="icon icon-arrow-right" />
-          </button>
-        </div>
-        {/* End arrow next */}
-      </div>
-    </>
+    //     spaceBetween={20}
+    //     modules={[Navigation, Pagination]}
+    //     navigation={{
+    //       nextEl: ".js-popular-brand-next",
+    //       prevEl: ".js-popular-brand-prev",
+    //     }}
+    //     pagination={{
+    //       el: ".js-brand-pag_active",
+    //       clickable: true,
+    //     }}
+    //     breakpoints={{
+    //       500: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 20,
+    //       },
+    //       768: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 22,
+    //       },
+    //       1024: {
+    //         slidesPerView: 3,
+    //       },
+    //       1200: {
+    //         slidesPerView: 4,
+    //       },
+    //     }}
+    //   >
+    //     {brandDetails?.manufacturers?.slice(0, 8).map((item) => (
+    //       <SwiperSlide key={item.id + item.name}>
+    //         <Link
+    //           href={`/brands/${item.id}`}
+    //           // className="carCard -type-1 d-block rounded-4 hover-inside-slider"
+    //           className="citiesCard -type-3 d-block text-center"
+    //           data-aos="fade"
+    //           // data-aos-delay={item.delayAnimation}
+    //         >
+    //           <div
+    //             key={item.id + item.name}
+    //             data-aos="fade"
+    //             // data-aos-delay={item?.delayAnimation}
+    //             className="brand-card"
+    //           >
+    //             <div
+    //               //  className="carCard__image truck-card-zoom"
+    //               className="brand-card__image rounded-full mx-auto"
+    //             >
+    //               <img
+    //                 width={300}
+    //                 height={300}
+    //                 className="js-lazy"
+    //                 src={item.logo[0].thumbnail}
+    //                 alt="image"
+    //               />
+    //             </div>
+    //           </div>
+    //         </Link>
+    //       </SwiperSlide>
+    //     ))}
+    //   </Swiper>
+
+    //   <div className="d-flex x-gap-15 items-center justify-center pt-20 sm:pt-20">
+    //     <div className="col-auto">
+    //       <button className="d-flex items-center text-24 arrow-left-hover js-popular-brand-prev">
+    //         <i className="icon icon-arrow-left" />
+    //       </button>
+    //     </div>
+    //     {/* End arrow prev */}
+
+    //     <div className="col-auto">
+    //       <div className="pagination -dots text-border js-brand-pag_active" />
+    //     </div>
+    //     {/* End arrow pagination */}
+
+    //     <div className="col-auto">
+    //       <button className="d-flex items-center text-24 arrow-right-hover js-popular-brand-next">
+    //         <i className="icon icon-arrow-right" />
+    //       </button>
+    //     </div>
+    //     {/* End arrow next */}
+    //   </div>
+    // </>
   );
 };
 
