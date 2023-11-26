9cba4597eef1ba858ae821f4b80284735f7452d2
akanandhu <akananthu11@gmail.com>
2023-11-26 14:15:36 +0530

    chore: remove third vehicle if mobile

--- components/compare/CompareDataTable.jsx
+++ components/compare/CompareDataTable.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 const CompareDataTable = ({ hasVariant, tableData }) => {
+  const isClient = typeof window !== "undefined";
+  const screenWidth = isClient ? window.innerWidth : null;
+  const isMobScreen = screenWidth < 576;
+
   return (
     <div className="overflow-scroll scroll-bar-1">
       <table className="table-3 -border-bottom col-12">
@@ -10,7 +14,7 @@ const CompareDataTable = ({ hasVariant, tableData }) => {
               <th></th>
               {hasVariant && <th>Vehicle 1</th>}
               {hasVariant && <th>Vehicle 2</th>}
-              {hasVariant && <th>Vehicle 3</th>}
+              {hasVariant && !isMobScreen && <th>Vehicle 3</th>}
             </tr>
           </thead>
         )}
@@ -21,7 +25,7 @@ const CompareDataTable = ({ hasVariant, tableData }) => {
               <td>{row.item}</td>
               <td>{row.option_one}</td>
               {row?.option_two && <td>{row.option_two}</td>}
-              {row?.option_three && <td>{row.option_three}</td>}
+              {row?.option_three && !isMobScreen && <td>{row.option_three}</td>}
             </tr>
           ))}
         </tbody>
