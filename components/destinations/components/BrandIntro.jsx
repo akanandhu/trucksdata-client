75a66ae634ea6c3902fb8cf8e4355837e3f731d2
akanandhu <akananthu11@gmail.com>
2024-01-11 09:59:17 +0530

    chore: add description view more

--- components/destinations/components/BrandIntro.jsx
+++ components/destinations/components/BrandIntro.jsx
@@ -2,26 +2,32 @@ import { useState } from "react";
 
 const BrandIntro = ({ description }) => {
   const [showMore, setShowMore] = useState(false);
-
+  const maxLength = 545;
+  const truncatedDescription = description?.substring(0, maxLength);
   return (
     <>
-      {description ? <div className="col-xl-8">
-        <p className="text-15 text-dark-1">
-          {showMore ? description : `${description?.substring(0, 545)}`}
-        </p>
-        <a
-          // href="#"
-          className="d-block text-14 fw-500 text-blue-1 underline mt-20"
-          onClick={() => setShowMore(!showMore)}
-        >
-          {description.split(" ").length > 545 ? (
-            <span>
-              {" "}
-              showMore ? <span>Show less</span> : <span>Show more</span>
-            </span>
-          ) : null}
-        </a>
-      </div>:null}
+      {description ? (
+        <div className="col-xl-8">
+          <p className="text-15 text-dark-1">
+            {showMore ? description : truncatedDescription}
+            {description.length > maxLength && (
+              <span>
+                {" "}
+                <a
+                  href="#"
+                  className="text-14 fw-500 text-blue-1 underline"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setShowMore(!showMore);
+                  }}
+                >
+                  {showMore ? " Show less" : " Show more"}
+                </a>
+              </span>
+            )}
+          </p>
+        </div>
+      ) : null}
     </>
   );
 };
