27b9f0681468c6806847d70eae5524ea43e4b6bb
akanandhu <akananthu11@gmail.com>
2024-02-07 23:08:04 +0530

    feat: filters multiple select

--- components/Filter/SearchFilter/FilterButtons.jsx
+++ components/Filter/SearchFilter/FilterButtons.jsx
@@ -8,17 +8,28 @@ const ButtonFilter = ({
   specId,
 }) => {
   // for start and guest rating code
-  const [activeRating, setActiveRating] = useState(0);
+  const [activeRating, setActiveRating] = useState([]);
   const [refresh, setRefresh] = useState(0);
 
   const handleRatingClick = (rating) => {
-    const newRating = activeRating === rating ? 0 : rating;
-    setActiveRating(newRating);
+    const newRating = rating
+    if(activeRating.includes(newRating)) {
+      const updatedRatingArray = activeRating.filter((r) => r !== newRating);
+    setActiveRating(updatedRatingArray);
     setSideParams({
       ...sideParams,
       [label]: newRating,
       [`${label}_spec_id`]: specId,
     });
+    } else {
+      setActiveRating([...activeRating, rating]);
+      setSideParams({
+        ...sideParams,
+        [label]: [...activeRating, rating],
+        [`${label}_spec_id`]: specId,
+      });
+    }
+    
     setRefresh((refresh) => refresh + 1);
   };
 
@@ -32,7 +43,7 @@ const ButtonFilter = ({
               <button
                 key={`refresh_${refresh}`}
                 className={`button -blue-1 bg-blue-1-05 text-blue-1 py-10 px-20 rounded-100 ${
-                  activeRating === filter.value
+                  activeRating?.includes(filter.value)
                     ? "active"
                     : activeRating === 0
                     ? "none"
